710548d25f7f6fa569a0fb38ad689c093b57ef14
aravindmedamoni <aravnd.accurate@gmail.com>
2020-03-01 02:36:34 +0530

    button shape changed through  kotlin code..

--- app/src/main/java/com/example/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val round = RoundedBitmapDrawableFactory.create(resources,bitmap)
+        round.cornerRadius = 18f
+        profile.setImageDrawable(round)
     }
 }
